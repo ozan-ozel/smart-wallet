54347692c7d2241ca030ce5fcb400e3423b2bb5b
Ozan Ozel <ozandozel@gmail.com>
2023-03-05 11:52:57 +0300

    Files formatted, type orm config init added to app module file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: process.env.POSTGRES_DB_HOST,
+			port: Number(process.env.POSTGRES_DB_PORT),
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+			entities: [],
+			synchronize: false,
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
